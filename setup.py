16f1ea438052abcd169123f241ad0399783a398a
requires.io <support@requires.io>
2017-04-05 08:46:12 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.2.3'
+version = '0.2.4'
 
 setup(
     name='django-docopt-command',
@@ -23,6 +23,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy"
     ]
