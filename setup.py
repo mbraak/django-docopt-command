18af2dec47b3961c07c18749023b4a19498a7c8a
Marco Braak <marco@invoicefinance.nl>
2019-02-15 08:33:43 +0100

    Test with Django 2.2b1; also add Django 2.2 to setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 2.7",
